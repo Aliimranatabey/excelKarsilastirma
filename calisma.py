6f6e14555efd931679b07d593bbd769926621a07
Aliimranatabey <aliimranatabey@gmail.com>
2024-02-08 16:13:15 +0300

    son calisma eklendi

--- calisma.py
+++ calisma.py
@@ -1,47 +1,51 @@
-import pandas as pd
-import numpy as np
+import openpyxl as op
+
 # Excel dosyasının yolu
-excel_dosya_yolu = 'tryExcel.xlsx'  
+dosya1 = op.load_workbook('5Ghz_Kapali_Cihaz_Listesi.xlsx')   
+sheet1=dosya1.active
+dosya2 = op.load_workbook('30_Gun_Uzerinde_Resetlenmemis_Cihaz_Listesi.xlsx')
+sheet2=dosya2.active
 
-# Excel dosyasını oku
-veri_cercevesi = pd.read_excel(excel_dosya_yolu)                               
-
-# Rastgele sayılar ürettik
-rastgele_sayilar = np.random.randint(low=1, high=30, size=20)
+output=op.load_workbook('tryExcel.xlsx') 
+outputSheet=output.active                               
+  
+# Veri çerçevesinde belirli bir sütunu seçtik
+macColumn1=sheet1['G']
+macColumn1_List=[]
+uzunlukVeri1=0
+uzunluk1=len(macColumn1)
+for row in macColumn1:
+    uzunlukVeri1=uzunlukVeri1+1
+    print("veri1 "+ str(uzunlukVeri1) + "/" + str(uzunluk1))
+    macColumn1_List.append(row.value)
 
 # Veri çerçevesinde belirli bir sütunu seçtik
-sutun_adi1 = veri_cercevesi.columns[0]  # Sütun adını değiştirdik
-veri_cercevesi[sutun_adi1]=rastgele_sayilar
-veri_cercevesi.to_excel(excel_dosya_yolu, index=False)
-# Rastgele sayılar üret
-rastgele_sayilar = np.random.randint(low=1, high=30, size=20)
-sutun_adi2 = veri_cercevesi.columns[1]  
-# Veri çerçevesinde belirli bir sütunu seçtik
-veri_cercevesi[sutun_adi2]=rastgele_sayilar
-# Veriyi güncellendikten sonra Excel dosyasına yazdık
-veri_cercevesi.to_excel(excel_dosya_yolu, index=False)
+macColumn2=sheet2['I']
+macColumn2_List=[]
+uzunlukVeri2=0
+uzunluk2=len(macColumn2)
+for row in macColumn2:
+    uzunlukVeri2=uzunlukVeri2+1
+    print("veri2 "+ str(uzunlukVeri2) + "/" + str(uzunluk2))
+    macColumn2_List.append(row.value)
 
-# Veri çerçevesini görüntüledik
-print("Güncellenmiş Veri Çerçevesi:")
-print(veri_cercevesi)
 
-bireBirAyniDeger = sum(veri_cercevesi[sutun_adi1] == veri_cercevesi[sutun_adi2])
+compare_list=[]
+uzunluk=len(macColumn1_List)
+basla=0
+for i in macColumn1_List:
+    basla=basla+1
+    print(str(basla)+ "/" +str(uzunluk))
+    if i in macColumn2_List:
+        if i not in compare_list:
+            compare_list.append(i)
 
-i=1
-aynıDegerDizesi=[]
-while i<len(veri_cercevesi[sutun_adi1]):
-    j=1
-    while j<len(veri_cercevesi[sutun_adi1]):
-        if veri_cercevesi[sutun_adi1][i]==veri_cercevesi[sutun_adi2][j]:  
-            l=1
-            while l<len(veri_cercevesi[sutun_adi1]):
-                if veri_cercevesi[sutun_adi1][i]==veri_cercevesi[sutun_adi1][l]:
-                    aynıDegerDizesi.append(veri_cercevesi[sutun_adi1][i])
-                l=l+1
-            print("Aynı Degerler : ",veri_cercevesi[sutun_adi1][i])
-        j=j+1
-    i=i+1
+print(compare_list)
+excel_l=range(len(compare_list))
+        
 
-benzersizElemanlar=set(aynıDegerDizesi)
-print("Aynı Deger Dizesi",benzersizElemanlar," Adedi : ",len(benzersizElemanlar))
-print("İki sütunun birebir aynı değere sahip satırlarının adedi : ",bireBirAyniDeger) 
+for i, j in zip(compare_list, excel_l):
+    c1 = outputSheet.cell(row = j+1, column = 1) 
+    c1.value = i
+
+output.save('tryExcel.xlsx')
---
+++ öncekidenemelerim.py
@@ -0,0 +1,41 @@
+# benzersizElemanlar=set(aynıDegerDizesi)
+# print("Aynı Deger Dizesi",benzersizElemanlar," Adedi : ",len(benzersizElemanlar))
+# output.save('tryExcel.xlsx')
+# print("HelloS10")
+
+
+# i=0
+# aynıDegerDizesi=[]
+# print("Hello")
+# while i<len(outputSheet['A']):
+#     j=0
+#     while j<len(outputSheet['B']):
+#         if outputSheet['A'][i]==outputSheet['B'][j]:  
+#             l=0
+#             while l<len(outputSheet['A']):
+#                 if outputSheet['A'][i]==outputSheet['A'][l]:
+#                     aynıDegerDizesi.append(outputSheet['A'][i])
+#                 l=l+1
+#                 print("Aynı Degerler : ",outputSheet['A'][i])
+            
+#         j=j+1
+#     i=i+1
+# print("Hello 3")
+# # benzersizElemanlar=set(aynıDegerDizesi)
+# # print("Aynı Deger Dizesi",benzersizElemanlar," Adedi : ",len(benzersizElemanlar))
+# # output.save('tryExcel.xlsx')
+# # print("İki sütunun birebir aynı değere sahip satırlarının adedi : ",bireBirAyniDeger) 
+
+"""
+for i in sutunA:
+    for j in sutunB:
+        if i == j:
+            aynıDegerDizesi.append(i)
+            print("Aynı degerler : ",i)
+
+"""
+
+
+
+    # outputSheet['A'+str(row.row)]=row.value
+    # outputSheet['B'+str(row.row)]=row.value
